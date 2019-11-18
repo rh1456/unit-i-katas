5f2ea719d08850719eb45f03752657e1684f156c
Reid Hanson <reidhans@buffalo.edu>
2019-11-17 23:57:11 -0500

    tests

--- src/functions.test.js
+++ src/functions.test.js
@@ -20,19 +20,40 @@
  *
  * i.e. findLongestWord("book dogs") should return "book"
  */
-// ...
+const findLongestWord = string => {
+  let longestWord = ''
+  const words = string.split(' ')
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].length > longestWord.length) {
+      longestWord = words[i]
+    }
+  }
+  return longestWord
+}
 
 /*
  * 2) Define a function sumOfArray that calculates the sum of
  * all the numbers in an array.
  */
-
+const sumOfArray = numbers => {
+  return numbers.reduce((total, number) => {
+    return (total += number)
+  }, 0)
+}
 /**
  *
  * 3) Define a function called doesContain that takes 2 arguments, an array and a value. 
  *    Check whether the provided array contains the value. Array can contain numbers or strings.
       Your function should return true if the array contains the value, false if not.
  */
+const doesContain = (array, value) => {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === value) {
+      return true
+    }
+  }
+  return false
+}
 
 /**
  * 4) defined a function called maps. This takes 1 argument, an array or numbers. 
@@ -44,14 +65,21 @@
    Try to use the map method - it comes in very handy quite a lot so is a good one to know.
  *    
  */
+const maps = numbers => {
+  return numbers.map(number => {
+    return number * 2
+  })
+}
 
 /**
- * 5) Write a function called noSpace. This function takes one argument, a string. 
- *    This function should return the string with no spaces. 
- *    For example: 
+ * 5) Write a function called noSpace. This function takes one argument, a string.
+ *    This function should return the string with no spaces.
+ *    For example:
  *    noSpace("This is a my cool thing") should return "Thisisamycoolthing"
-
-
+ */
+const noSpace = string => {
+  return string.replace(/ /g, '')
+}
 /**
  * NOTE: Don't modify anything below this line...
  */
